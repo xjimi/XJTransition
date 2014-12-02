28fd52bc01b0d4396329d406ba5801fd4975fe78
xjimi <fn5128@gmail.com>
2014-12-02 11:04:43 +0800

    Initial Commit

---
+++ XJTransitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJTransitionDemo
+//
+//  Created by jimi on 2014/12/2.
+//  Copyright (c) 2014年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJTransitionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJTransitionDemo
+//
+//  Created by jimi on 2014/12/2.
+//  Copyright (c) 2014年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
